c3860aae9da39c671c86ad32ed5f73091e8a7911
Ulf Magnusson <ulfalizer@gmail.com>
2013-10-19 18:17:58 +0200

    Enter debugger with F8 instead of D.
    Harder to hit by accident.

--- cpu.cpp
+++ cpu.cpp
@@ -1595,7 +1595,7 @@ static void print_state() {
 
 static void log_instruction() {
     if (debug_mode == RUN) {
-        if ((n_breakpoints_set > 0 && breakpoint_at[pc]) || keys[SDL_SCANCODE_D])
+        if ((n_breakpoints_set > 0 && breakpoint_at[pc]) || keys[SDL_SCANCODE_F8])
             debug_mode = SINGLE_STEP;
         else
             return;
